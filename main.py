eb90f4a46942cc24598ebaa977bf6974eed66648
Napat Charoenlarpkul <longman_694@hotmail.com>
2025-11-22 19:38:13 +0700

    completed main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from lib.trim import remove_header_batch
+from lib.ocr import run_ocr_pipeline
+from lib.epub import convert_md_to_epub
+
+
 def main():
-    print("Hello from ocr!")
+    print("Run trimming...")
+    remove_header_batch()
+    print("====================")
+    
+    print("Run OCR...")
+    run_ocr_pipeline()
+    print("====================")
+
+    print("Run create EPUB")
+    convert_md_to_epub()
+    print("====================")
 
 
 if __name__ == "__main__":
